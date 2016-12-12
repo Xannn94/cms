56a293c2564c910ec6de889ebc1b620bd59948bc
xannn94@mail.ru <0773385829a>
2016-12-12 17:18:10 +0600

    да да коммит

---
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 07.12.2016
+ * Time: 10:54
+ */
+
+namespace App\Http\Middleware;
+
+
+class AdminAuthenticate
+{
+
+}
---
+++ app/Model/Ticket.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 12.12.2016
+ * Time: 13:49
+ */
